e5bf72e4d437cc69354633559d8d7374a330340b
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2025-04-12 08:00:33 +0200

    Elm example/solution: add a 2nd dialog

--- pfm-elm/src/Main2.elm
+++ pfm-elm/src/Main2.elm
@@ -22,11 +22,16 @@ port closeDialog : () -> Cmd msg
 
 type alias Model =
     { tableData : List Person
-    , dialog : Maybe DialogForm
+    , dialog : Maybe Dialog
     }
 
 
-type alias DialogForm =
+type Dialog
+    = Dialog1 DialogForm1
+    | Dialog2 DialogForm2
+
+
+type alias DialogForm1 =
     { input1 : String
     , select1 : String
     , input2 : String
@@ -34,12 +39,27 @@ type alias DialogForm =
     }
 
 
-emptyForm : DialogForm
-emptyForm =
+type alias DialogForm2 =
+    { name : String
+    , email : String
+    , comment : String
+    }
+
+
+emptyForm1 : DialogForm1
+emptyForm1 =
     { input1 = ""
     , select1 = "option1"
     , input2 = ""
     , select2 = "option3"
+    }
+
+
+emptyForm2 : DialogForm2
+emptyForm2 =
+    { name = ""
+    , email = ""
+    , comment = ""
     }
 
 
@@ -66,23 +86,36 @@ init _ =
 
 
 type Msg
-    = ShowDialog
+    = ShowDialog1
+    | ShowDialog2
     | CloseDialog
-    | DialogFormMsg DialogFormMsg
+    | Dialog1FormMsg Dialog1FormMsg
+    | Dialog2FormMsg Dialog2FormMsg
 
 
-type DialogFormMsg
+type Dialog1FormMsg
     = Input1Changed String
     | Select1Changed String
     | Input2Changed String
     | Select2Changed String
 
 
+type Dialog2FormMsg
+    = NameChanged String
+    | EmailChanged String
+    | CommentChanged String
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ShowDialog ->
-            ( { model | dialog = Just emptyForm }
+        ShowDialog1 ->
+            ( { model | dialog = Just (Dialog1 emptyForm1) }
+            , showDialog ()
+            )
+
+        ShowDialog2 ->
+            ( { model | dialog = Just (Dialog2 emptyForm2) }
             , showDialog ()
             )
 
@@ -91,19 +124,29 @@ update msg model =
             , closeDialog ()
             )
 
-        DialogFormMsg formMsg ->
+        Dialog1FormMsg formMsg ->
             case model.dialog of
-                Just form ->
-                    ( { model | dialog = Just (updateDialogForm formMsg form) }
+                Just (Dialog1 form) ->
+                    ( { model | dialog = Just (Dialog1 (updateDialog1Form formMsg form)) }
                     , Cmd.none
                     )
 
-                Nothing ->
+                _ ->
+                    ( model, Cmd.none )
+
+        Dialog2FormMsg formMsg ->
+            case model.dialog of
+                Just (Dialog2 form) ->
+                    ( { model | dialog = Just (Dialog2 (updateDialog2Form formMsg form)) }
+                    , Cmd.none
+                    )
+
+                _ ->
                     ( model, Cmd.none )
 
 
-updateDialogForm : DialogFormMsg -> DialogForm -> DialogForm
-updateDialogForm msg form =
+updateDialog1Form : Dialog1FormMsg -> DialogForm1 -> DialogForm1
+updateDialog1Form msg form =
     case msg of
         Input1Changed value ->
             { form | input1 = value }
@@ -116,6 +159,19 @@ updateDialogForm msg form =
 
         Select2Changed value ->
             { form | select2 = value }
+
+
+updateDialog2Form : Dialog2FormMsg -> DialogForm2 -> DialogForm2
+updateDialog2Form msg form =
+    case msg of
+        NameChanged value ->
+            { form | name = value }
+
+        EmailChanged value ->
+            { form | email = value }
+
+        CommentChanged value ->
+            { form | comment = value }
 
 
 
@@ -131,8 +187,25 @@ view : Model -> Html Msg
 view model =
     H.div []
         [ viewTable model.tableData
-        , H.button [ HE.onClick ShowDialog ] [ H.text "Open Dialog" ]
-        , whenJust model.dialog viewDialog
+        , H.div [ HA.style "margin" "10px 0" ]
+            [ H.button 
+                [ HE.onClick ShowDialog1
+                , HA.style "margin-right" "10px" 
+                ] 
+                [ H.text "Open Dialog 1" ]
+            , H.button 
+                [ HE.onClick ShowDialog2 ] 
+                [ H.text "Open Dialog 2" ]
+            ]
+        , case model.dialog of
+            Just (Dialog1 form) ->
+                viewDialog1 form
+
+            Just (Dialog2 form) ->
+                viewDialog2 form
+
+            Nothing ->
+                H.text ""
         ]
 
 
@@ -157,26 +230,54 @@ viewTableRow person =
         ]
 
 
-viewDialog : DialogForm -> Html Msg
-viewDialog form =
+viewDialog1 : DialogForm1 -> Html Msg
+viewDialog1 form =
     H.node "dialog"
         [ HA.id "myDialog" ]
         [ H.div [ HA.class "dialog-content" ]
-            [ H.h3 [] [ H.text "Dialog Form" ]
+            [ H.h3 [] [ H.text "Dialog 1 Form" ]
             , H.label [ HA.for "input1" ] [ H.text "Input 1:" ]
-            , H.input [ HA.type_ "text", HA.id "input1", HE.onInput (DialogFormMsg << Input1Changed), HA.value form.input1 ] []
+            , H.input [ HA.type_ "text", HA.id "input1", HE.onInput (Dialog1FormMsg << Input1Changed), HA.value form.input1 ] []
             , H.label [ HA.for "select1" ] [ H.text "Select 1:" ]
-            , H.select [ HA.id "select1", HE.onInput (DialogFormMsg << Select1Changed) ]
+            , H.select [ HA.id "select1", HE.onInput (Dialog1FormMsg << Select1Changed) ]
                 [ H.option [ HA.value "option1", HA.selected (form.select1 == "option1") ] [ H.text "Option 1" ]
                 , H.option [ HA.value "option2", HA.selected (form.select1 == "option2") ] [ H.text "Option 2" ]
                 ]
             , H.label [ HA.for "input2" ] [ H.text "Input 2 (Target Focus):" ]
-            , H.input [ HA.type_ "text", HA.id "input2", HE.onInput (DialogFormMsg << Input2Changed), HA.value form.input2, HA.attribute "autofocus" "" ] []
+            , H.input [ HA.type_ "text", HA.id "input2", HE.onInput (Dialog1FormMsg << Input2Changed), HA.value form.input2, HA.attribute "autofocus" "" ] []
             , H.label [ HA.for "select2" ] [ H.text "Select 2:" ]
-            , H.select [ HA.id "select2", HE.onInput (DialogFormMsg << Select2Changed) ]
+            , H.select [ HA.id "select2", HE.onInput (Dialog1FormMsg << Select2Changed) ]
                 [ H.option [ HA.value "option3", HA.selected (form.select2 == "option3") ] [ H.text "Option 3" ]
                 , H.option [ HA.value "option4", HA.selected (form.select2 == "option4") ] [ H.text "Option 4" ]
                 ]
+            , H.button
+                [ HA.id "closeDialog"
+                , HE.onClick CloseDialog
+                , HA.style "margin-top" "10px"
+                ]
+                [ H.text "Close" ]
+            ]
+        ]
+
+
+viewDialog2 : DialogForm2 -> Html Msg
+viewDialog2 form =
+    H.node "dialog"
+        [ HA.id "myDialog" ]
+        [ H.div [ HA.class "dialog-content" ]
+            [ H.h3 [] [ H.text "Dialog 2 - Feedback Form" ]
+            , H.label [ HA.for "name" ] [ H.text "Name:" ]
+            , H.input [ HA.type_ "text", HA.id "name", HE.onInput (Dialog2FormMsg << NameChanged), HA.value form.name ] []
+            , H.label [ HA.for "email" ] [ H.text "Email:" ]
+            , H.input [ HA.type_ "email", HA.id "email", HE.onInput (Dialog2FormMsg << EmailChanged), HA.value form.email, HA.attribute "autofocus" "" ] []
+            , H.label [ HA.for "comment" ] [ H.text "Comment:" ]
+            , H.textarea 
+                [ HA.id "comment"
+                , HE.onInput (Dialog2FormMsg << CommentChanged)
+                , HA.value form.comment
+                , HA.style "width" "100%"
+                , HA.style "min-height" "100px"
+                ] []
             , H.button
                 [ HA.id "closeDialog"
                 , HE.onClick CloseDialog
@@ -204,7 +305,7 @@ main : Program () Model Msg
 main =
     Browser.element
         { init = init
-        , view = view
         , update = update
         , subscriptions = subscriptions
+        , view = view
         }
