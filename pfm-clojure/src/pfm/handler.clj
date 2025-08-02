b5c3ad3f39e8146dca80a0bc3e68f07bc5260b40
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2025-08-02 22:09:18 +0200

    Use DI

--- pfm-clojure/src/pfm/handler.clj
+++ pfm-clojure/src/pfm/handler.clj
@@ -3,19 +3,27 @@
             [pfm.db :as db]
             [clojure.data.json :as json]))
 
-
+;; Pure handlers (no dependencies)
 (defn health-check [request]
   {:status 200
    :body "OK"})
 
-(defn get-transactions [request]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body (json/write-str (tx/get-all-transactions db/db-spec))})
+;; Handler factories (accept dependencies)
+(defn make-get-transactions-handler [get-all-transactions-fn]
+  (fn [request]
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body (json/write-str (get-all-transactions-fn))}))
 
-(defn app [request]
-  (case (:uri request)
-    "/health" (health-check request)
-    "/api/transactions" (get-transactions request)
-    {:status 404
-     :body "Not found"}))
+;; App factory - creates the main app with injected dependencies
+(defn make-app [deps]
+  (let [get-transactions (make-get-transactions-handler (:get-all-transactions deps))]
+    (fn [request]
+      (case (:uri request)
+        "/health" (health-check request)
+        "/api/transactions" (get-transactions request)
+        {:status 404
+         :body "Not found"}))))
+
+;; Default app with real dependencies for production use
+(def app (make-app {:get-all-transactions #(tx/get-all-transactions db/db-spec)}))
--- pfm-clojure/test/pfm/handler_test.clj
+++ pfm-clojure/test/pfm/handler_test.clj
@@ -2,7 +2,6 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [pfm.handler :as handler]
-            [pfm.db.transaction :as db.tx]
             [clojure.data.json :as json]))
 
 (deftest health-check-test
@@ -14,27 +13,44 @@
 
 (deftest app-test
   (testing "GET /wat should return a 404"
-    (let [response (handler/app (mock/request :get "/wat"))]
+    (let [test-app (handler/make-app {:get-all-transactions (constantly [])})
+          response (test-app (mock/request :get "/wat"))]
       (is (= 404 (:status response)))
       (is (= "Not found" (:body response))))))
 
-(deftest get-transactions-test
+(deftest get-transactions-handler-test
   (testing "GET /api/transactions returns empty JSON when no transactions"
-    (with-redefs [db.tx/get-all-transactions (constantly [])]
-      (let [response (handler/get-transactions {})
-            parsed-body (json/read-str (:body response))]
-        (is (= 200 (:status response)))
-        (is (= "application/json" (get-in response [:headers "Content-Type"])))
-        (is (= [] parsed-body)))))
+    (let [handler (handler/make-get-transactions-handler (constantly []))
+          response (handler {})
+          parsed-body (json/read-str (:body response))]
+      (is (= 200 (:status response)))
+      (is (= "application/json" (get-in response [:headers "Content-Type"])))
+      (is (= [] parsed-body))))
   
   (testing "GET /api/transactions returns actual transaction data"
     (let [test-data [{:id 1 :description "Coffee" :amount 4.50 :created_at_unix 1234567890}
-                     {:id 2 :description "Lunch" :amount 12.75 :created_at_unix 1234567891}]]
-      (with-redefs [db.tx/get-all-transactions (constantly test-data)]
-        (let [response (handler/get-transactions {})
+                     {:id 2 :description "Lunch" :amount 12.75 :created_at_unix 1234567891}]
+          handler (handler/make-get-transactions-handler (constantly test-data))
+          response (handler {})
+          parsed-body (json/read-str (:body response) :key-fn keyword)]
+      (is (= 200 (:status response)))
+      (is (= 2 (count parsed-body)))
+      (is (= "Coffee" (:description (first parsed-body))))
+      (is (= 4.5 (:amount (first parsed-body))))
+      (is (= "Lunch" (:description (second parsed-body)))))))
+
+(deftest app-integration-test
+  (testing "App routes requests correctly"
+    (let [test-data [{:id 1 :description "Test"}]
+          test-app (handler/make-app {:get-all-transactions (constantly test-data)})]
+      
+      (testing "health endpoint"
+        (let [response (test-app (mock/request :get "/health"))]
+          (is (= 200 (:status response)))
+          (is (= "OK" (:body response)))))
+      
+      (testing "transactions endpoint"
+        (let [response (test-app (mock/request :get "/api/transactions"))
               parsed-body (json/read-str (:body response) :key-fn keyword)]
           (is (= 200 (:status response)))
-          (is (= 2 (count parsed-body)))
-          (is (= "Coffee" (:description (first parsed-body))))
-          (is (= 4.5 (:amount (first parsed-body))))
-          (is (= "Lunch" (:description (second parsed-body)))))))))
+          (is (= test-data parsed-body)))))))
