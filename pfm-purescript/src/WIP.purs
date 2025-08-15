7026e54a680487b9ee7136c64b05635459303b15
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2025-08-15 06:56:45 +0200

    Use launchAff_

--- pfm-purescript/src/WIP.purs
+++ pfm-purescript/src/WIP.purs
@@ -2,48 +2,41 @@ module WIP where
 
 import Prelude
 
-import Data.Array ((!!))
 import Data.Array as Array
-import Data.Either (either)
 import Data.Traversable (traverse_)
 import Effect (Effect)
-import Effect.Aff (Aff, runAff_)
-import Effect.Class.Console (log, logShow)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Class.Console (logShow)
 import SQLite3 as SQLite3
 import Server.DB.LedgerView.Queries as LedgerViewQueries
-import Server.DB.Transactions.Queries (TransactionDB(..))
-import Server.Database (seedFromOfx)
-import Server.Database as DB
 import Server.DB.Transactions.Queries as TransactionQueries
-import Shared.Types (Transaction(..))
+import Server.Database (seedFromOfx)
 
 {-
 
 Usage
 =====
 
-repl> run $ newConn >>= DB.getSomething
-repl> newConn >>= DB.getSomething # run
+repl> launchAff_ $ newConn >>= DB.getSomething
+repl> newConn >>= DB.getSomething # launchAff_
 
 ---
 
--- run $ newConn >>= DB.getAllUsers
--- run $ newConn >>= DB.seedFromOfx ".tmp/CA20250630_124433.ofx"
--- run $ newConn >>= DB.getBudgetIdForDate 1719792000
--- run $ newConn >>= DB.getAllTransactions >>= traverse_ logShow
--- run $ newConn >>= DB.getAllTransactions <#> (_ !! 5) >>= traverse_ logShow
--- run $ newConn >>= DB.getAllTransactions <#> Array.take 3 >>= traverse_ logShow
--- run $ newConn >>= DB.getAllTransactions <#> Array.drop 3 <#> Array.take 3 >>= traverse_ logShow
--- run $ newConn >>= DB.getLedgerViewRows <#> Array.drop 3 <#> Array.take 3 >>= traverse_ logShow
+repl> import Server.DB.LedgerView.Queries as LedgerViewQueries
+repl> launchAff_ $ newConn >>= LedgerViewQueries.getLedgerViewRows 4 <#> Array.drop 3 <#> Array.take 3 >>= traverse_ logShow
+
+---
+
+-- launchAff_ $ newConn >>= DB.getAllUsers
+-- launchAff_ $ newConn >>= DB.seedFromOfx ".tmp/CA20250630_124433.ofx"
+-- launchAff_ $ newConn >>= DB.getBudgetIdForDate 1719792000
+-- launchAff_ $ newConn >>= DB.getAllTransactions >>= traverse_ logShow
+-- launchAff_ $ newConn >>= DB.getAllTransactions <#> (_ !! 5) >>= traverse_ logShow
+-- launchAff_ $ newConn >>= DB.getAllTransactions <#> Array.take 3 >>= traverse_ logShow
+-- launchAff_ $ newConn >>= DB.getAllTransactions <#> Array.drop 3 <#> Array.take 3 >>= traverse_ logShow
+-- launchAff_ $ newConn >>= DB.getLedgerViewRows <#> Array.drop 3 <#> Array.take 3 >>= traverse_ logShow
 
  -}
-run :: forall a. Show a => Aff a -> Effect Unit
-run =
-  runAff_
-    ( either
-        (\err -> log $ "Error: " <> show err)
-        (log <<< show)
-    )
 
 -- Database connection with foreign keys enabled
 newConn :: Aff SQLite3.DBConnection
@@ -52,12 +45,23 @@ newConn = do
   _ <- SQLite3.queryDB conn "PRAGMA foreign_keys = ON" []
   pure conn
 
--- Seed database from OFX file
+{-
+
+Via the REPL:
+  repl> seed
+
+Run as a script (faster log feedback)
+  node -e "import('./output/WIP/index.js').then(m => m.seed())"
+
+ -}
 seed :: Effect Unit
-seed = run $ newConn >>= seedFromOfx ".tmp/CA20250630_124433.ofx"
+seed = launchAff_ $ newConn >>= seedFromOfx ".tmp/CA20250630_124433.ofx"
 
 showTransactions :: Effect Unit
 showTransactions =
-  run $ newConn >>= TransactionQueries.getAllTransactions >>= traverse_ logShow
+  launchAff_ $ newConn >>= TransactionQueries.getAllTransactions >>= traverse_ logShow
 
+-- launchAff_ wip
+wip ∷ Aff Unit
 wip = newConn >>= LedgerViewQueries.getLedgerViewRows 4 <#> Array.drop 3 <#> Array.take 3 >>= traverse_ logShow
+
